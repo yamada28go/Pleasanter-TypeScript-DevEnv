013a8d8fcaab35229cd4f951f7b00cef49e4687d
yamada28go <yamada28go@gmail.com>
2024-02-04 11:40:09 +0900

    公式のパッケージ形式に変換

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Test");
+// サンプルコード
+$p.setMessage(
+  "#Message",
+  JSON.stringify({
+    Css: "alert-success",
+    Text: "処理が正常に終了しました。",
+  }as SetMessageArgumentType)
+);
+
+// $p.apiGet({
+//   id: 1545,
+//   done: (data) => {
+//     console.log(data);
+//     debugger;
+//   },
+// });
